19d0faa255cd771cfdb0ef3bfdeabfffe61e6a4c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-01-28 22:38:40 -0300

    criando um json no próprio script

--- js/script.js
+++ js/script.js
@@ -1,246 +1,151 @@
-let selectSun = document.querySelector('.first-select');
-let selectWateringcan = document.querySelector('.second-select');
-let selectPets = document.querySelector('.third-select');
+const selectSun = document.querySelector(".first-select");
+const selectWateringcan = document.querySelector(".second-select");
+const selectPets = document.querySelector(".third-select");
+const footer = document.querySelector("footer");
+const results = document.querySelector(".results");
 let valueSelectSun;
 let valueSelectWateringcan;
 let valueSelectPets;
-let resetSelectSun = document.querySelector('#select-sun')
-let resetSelectWateringcan = document.querySelector('#select-wateringcan');
-let resetSelectPets = document.querySelector('#select-pets');
 
-init();
+const plantsOfAvailable = [
+  {
+    id: 1,
+    name: "Euphorbia eritrea",
+    sun: "high",
+    water: "rarely",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/euphorbia-eritrea.png",
+    price: 25,
+    toxicity: false,
+  },
+  {
+    id: 2,
+    name: "Succulent Bowl",
+    sun: "high",
+    water: "rarely",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/succulent-bowl.png",
+    price: 30,
+    toxicity: false,
+  },
+  {
+    id: 3,
+    name: "Bunny ears cacti",
+    sun: "high",
+    water: "rarely",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/bunny-ears-cacti.png",
+    price: 20,
+    toxicity: false,
+  },
+  {
+    id: 4,
+    name: "Ficus lyrata",
+    sun: "high",
+    water: "regularly",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/ficus-lyrata.png",
+    price: 30,
+    toxicity: false,
+  },
+  {
+    id: 5,
+    name: "Bamboo",
+    sun: "low",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/lucky-bamboo.png",
+    water: "regularly",
+    price: 15,
+    toxicity: false,
+  },
+  {
+    id: 6,
+    name: "Ponytail Palm",
+    sun: "low",
+    water: "regularly",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/ponytail-palm.png",
+    price: 50,
+    toxicity: false,
+  },
+  {
+    id: 7,
+    name: "Pilea peperomioides",
+    sun: "no",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/pilea-peperomioides.png",
+    water: "regularly",
+    price: 50,
+    toxicity: true,
+  },
+  {
+    id: 8,
+    name: "Calathea triostar",
+    sun: "no",
+    water: "daily",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/calathea-triostar.png",
+    price: 50,
+    toxicity: true,
+  },
+  {
+    id: 9,
+    name: "Monstera deliciosa",
+    sun: "no",
+    url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/monstera-deliciosa.png",
+    water: "daily",
+    price: 50,
+    toxicity: true,
+  },
+];
 
-selectSun.addEventListener('change', (event) => {
+selectSun.addEventListener("change", (event) => {
   valueSelectSun = event.target.value;
   console.log(valueSelectSun);
-  showAndHideDisplay();
-  apiConnect();
-
-  setTimeout(() => {
-    resetSelectSun.value = '';
-  }, 3000);
+  //showAndHideDisplay();
+  filterSelectSun();
 });
 
-selectWateringcan.addEventListener('change', (event) => {
+selectWateringcan.addEventListener("change", (event) => {
   valueSelectWateringcan = event.target.value;
   console.log(valueSelectWateringcan);
-  showAndHideDisplay();
-  apiConnect();
-
-  setTimeout(() => {
-    resetSelectWateringcan.value = '';
-  }, 3000);
+  //showAndHideDisplay();
 });
 
-selectPets.addEventListener('change', (event) => {
+selectPets.addEventListener("change", (event) => {
   valueSelectPets = event.target.value;
   console.log(valueSelectPets);
-  showAndHideDisplay();
-  apiConnect();
-
-  setTimeout(() => {
-    resetSelectPets.value = '';
-  }, 3000)
+  //showAndHideDisplay();
 });
+/*
+function showAndHideDisplay() {
+  if (
+    valueSelectSun === "no" ||
+    valueSelectSun === "low" ||
+    valueSelectSun === "high"
+  ) {
+    footer.style.display = "none";
+    results.style.display = "block";
+  } else {
+    footer.style.display = "block";
+    results.style.display = "none";
+  }
+
+  if (
+    valueSelectWateringcan === "regularly" ||
+    valueSelectWateringcan === "daily" ||
+    valueSelectWateringcan === "rarely"
+  ) {
+    footer.style.display = "none";
+    results.style.display = "block";
+  } else {
+    footer.style.display = "block";
+    results.style.display = "none";
+  }
+
+  if (valueSelectPets === "true" || valueSelectPets === "false") {
+    footer.style.display = "none";
+    results.style.display = "block";
+  } else {
+    footer.style.display = "block";
+    results.style.display = "none";
+  }
+}
+
 
 function init() {
-  document.querySelector('.results').style.display = 'none';
+  document.querySelector(".results").style.display = "none";
 }
-
-function showAndHideDisplay() {
-  if(valueSelectSun === 'Select...') {
-    document.querySelector('footer').style.display = 'block';
-    document.querySelector('.results').style.display = 'none';
-  }
-
-  if(valueSelectSun === 'no') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectSun === 'low') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectSun === 'high') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectWateringcan === 'Select...') {
-    document.querySelector('footer').style.display = 'block';
-    document.querySelector('.results').style.display = 'none';
-  }
-
-  if(valueSelectWateringcan === 'regularly') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectWateringcan === 'daily') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectWateringcan === 'rarely') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectPets === 'Select...') {
-    document.querySelector('footer').style.display = 'block';
-    document.querySelector('.results').style.display = 'none';
-  }
-
-  if(valueSelectPets === 'true') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-
-  if(valueSelectPets === 'false') {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.results').style.display = 'block';
-
-    setTimeout(() => {
-      document.querySelector('.results').style.display = 'none';
-      document.querySelector('footer').style.display = 'block';
-    }, 10000);
-  }
-}
-
-function apiConnect() {
-  fetch('../plants.json').then(response => response.json()).then((data) => {
-    data.map((item, key) => {
-      const plantData = {
-        id: item.id,
-        name: item.name,
-        sun: item.sun,
-        water: item.water,
-        pets: item.toxicity,
-        image: item.url,
-        price: item.price,
-      }
-
-      if(valueSelectSun === plantData.sun) {
-        const listProducts = document.querySelector('.list-products');
-
-        const products = document.createElement('li');
-        products.classList.add('products');
-        products.innerHTML = `<li key=${plantData.id}></li>`;
-        listProducts.appendChild(products);
-
-        const cardsElement = document.createElement('div');
-        products.appendChild(cardsElement);
-        cardsElement.classList.add('cards-elements');
-
-        const cardImage = document.createElement('img');
-        cardImage.classList.add('card-image');
-        cardImage.innerHTML = `<img src=${plantData.image} alt=${plantData.name} />`;
-
-        const cardName = document.createElement('span');
-        cardName.classList.add('product-name');
-        cardName.innerHTML = `<span>${plantData.name}</span>`
-
-        const cardPrice = document.createElement('span');
-        cardPrice.classList.add('price');
-        cardPrice.innerHTML = `<span>$${plantData.price}</span>`;
-
-        cardsElement.appendChild(cardImage);
-        cardsElement.appendChild(cardName);
-        cardsElement.appendChild(cardPrice);
-      }
-
-      if(valueSelectWateringcan === plantData.water) {
-        const listProducts = document.querySelector('.list-products');
-
-        const products = document.createElement('li');
-        listProducts.appendChild(products);
-        products.classList.add('products');
-
-        const cardsElement = document.createElement('div');
-        products.appendChild(cardsElement);
-        cardsElement.classList.add('cards-elements');
-
-        const cardImage = document.createElement('img');
-        cardImage.classList.add('card-image');
-        cardImage.innerHTML = `<img src=${plantData.image} alt=${plantData.name} />`;
-
-        const cardName = document.createElement('span');
-        cardName.classList.add('product-name');
-        cardName.innerHTML = `<span>${plantData.name}</span>`
-
-        const cardPrice = document.createElement('span');
-        cardPrice.classList.add('price');
-        cardPrice.innerHTML = `<span>${plantData.price}</span>`;
-
-        cardsElement.appendChild(cardImage);
-        cardsElement.appendChild(cardName);
-        cardsElement.appendChild(cardPrice);
-      }
-
-      if(valueSelectPets === plantData.pets) {
-        const listProducts = document.querySelector('.list-products');
-
-        const products = document.createElement('li');
-        listProducts.appendChild(products);
-        products.classList.add('products');
-
-        const cardsElement = document.createElement('div');
-        products.appendChild(cardsElement);
-        cardsElement.classList.add('cards-elements');
-
-        const cardImage = document.createElement('img');
-        cardImage.classList.add('card-image');
-        cardImage.innerHTML = `<img src=${plantData.image} alt=${plantData.name} />`;
-
-        const cardName = document.createElement('span');
-        cardName.classList.add('product-name');
-        cardName.innerHTML = `<span>${plantData.name}</span>`
-
-        const cardPrice = document.createElement('span');
-        cardPrice.classList.add('price');
-        cardPrice.innerHTML = `<span>${plantData.price}</span>`;
-
-        cardsElement.appendChild(cardImage);
-        cardsElement.appendChild(cardName);
-        cardsElement.appendChild(cardPrice);
-      }
-    });
-  });
-}
+*/
